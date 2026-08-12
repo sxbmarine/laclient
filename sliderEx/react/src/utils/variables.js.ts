d503f528e9ab346baef325ebf9d16e8b4d4be18c
sxbmarine <imbrais715@gmail.com>
2026-08-12 12:04:08 +0200

    Reducir archivos y eliminar referencias

--- sliderEx/react/src/utils/variables.js.ts
+++
@@ -1,7 +0,0 @@
-// 设置图层变量模式
-  document.addEventListener('DOMContentLoaded', function() {
-            const element_3_68 = document.getElementById('3_68');
-            if (element_3_68) {
-              element_3_68.setAttribute('data-collection-5720-155-mode', 'dark');
-            }
-  });
